a0de0ca72bfc928c3fa98ee5fa4725b86e9884b4
Waleed Nasir <33671736+Waleed-Nasir@users.noreply.github.com>
2019-05-27 16:49:32 +0500

    Update meter

--- SpeedoMeter.js
+++ SpeedoMeter.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Image, TouchableOpacity,Animated,Easing, StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
- ////////////////LinearGradient or Use React Native LinearGradient//////////////////////
 import { LinearGradient } from 'expo'
 let screenWidth = Dimensions.get('window').width;
 let screenHeight = Dimensions.get('window').height;
-
+// import Up from '../../assets/Up.png'
 export default class SpeedoMeter extends React.Component {
     constructor(props) {
         super(props);
         this.state={
-            Progress_Value:28
+            Progress_Value:0
         }
-        this.speedometerValue = new Animated.Value(50);
+        this.speedometerValue = new Animated.Value(100);
       }
   static navigationOptions = {
     header: null,
@@ -31,10 +30,16 @@ export default class SpeedoMeter extends React.Component {
     }
     return Math.min(Math.max(currentValue, minValue), maxValue);
   }
+  componentWillReceiveProps(nextProps){
+    const {totalRange } = nextProps
+   this.setState({
+     Progress_Value:totalRange
+   })
+  }
   render() {
       const rotate = this.speedometerValue.interpolate({
-        inputRange: [this.state.Progress_Value, 100],
-        outputRange: ['-90deg', '90deg'],
+        inputRange: [0, 100],
+        outputRange: ['-100deg', '100deg'],
       });
       const {size, deviceWidth }=this.props
       const currentSize = this.validateSize(300, screenWidth);
@@ -46,44 +51,54 @@ export default class SpeedoMeter extends React.Component {
           easing: Easing.linear,
         },
       ).start();
+      const valRange =this.state.Progress_Value <50?`-${60-this.state.Progress_Value}deg`:`${this.state.Progress_Value}deg`
+      const  { ShowTemp = 0} = this.props
     return (
         <View style={styles.container}>
         {/* <View style={styles.outercontainer}> */}
+       
         <LinearGradient
-                start={{ x: 0, y: 0.75 }} end={{ x: 1, y: 0.25 }}
-                colors={['#cb5949' ,  '#d17c70'  , '#7fa0c1' , '#79aad0']}
+                start={{ x: 0, y: 0.55 }} end={{ x: 1, y: 0.45 }}
+                colors={['#d25040' ,'#d25040' , '#d17c70'  , '#d17c70'  , '#7fa0c1' , '#79aad0']}
                 style={styles.outercontainer}
               >
         <View style={styles.innercontainer}>
+        {Math.round(Math.abs(this.state.Progress_Value)) == 0 &&<View style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center'}}>
+                    <Text>No Data Found</Text>
+                    </View>}
         <View  style={styles.range}>
         <Animated.View style={[styles.imageWrapper,
             {
               top: -(currentSize / 15),
-              transform: [{ rotate:'45deg' }],
+              transform: [{ rotate }],
             },
             {}]}
           >
             <Image
               style={[styles.image,
                 {
-                  width: currentSize,
+                  width: currentSize-40,
                   height: currentSize,
                 }, {}]}
-              source={require('Place Your image')}
+              source={require('../../assets/speedometer-needle.png')}
             />
+         
           </Animated.View>
         </View>
         </View>
        
         <View style={styles.bottomcontainer}>
        <View style={styles.bottomText}>
-        <Text style={styles.bottomTextrape}>Temp</Text>
+        <Text style={styles.bottomTextrape}>{ShowTemp?"-40F":"0"}</Text>
        </View>
        <View  style={styles.bottomMiddle}>
-        <Text style={styles.bottomMiddlerape}>28%</Text>
+        <Text style={styles.bottomMiddlerapeText}>{ShowTemp?"Temperature":"Humidity"}</Text>
+       <Text style={styles.bottomMiddlerape}>{Math.round(Math.abs(this.state.Progress_Value))}{ShowTemp?'F':'%'}
+       <Image source={Up}  style={{width:20,height:20}}/>
+       </Text>
        </View>
        <View style={styles.bottomText}>
-        <Text style={styles.bottomTextrape}>Hum</Text>
+        <Text style={styles.bottomTextrape}>{ShowTemp?"134F":"100"}</Text>
        </View>
         </View>
         {/* </View> */}
@@ -102,26 +117,35 @@ export default class SpeedoMeter extends React.Component {
 const styles = StyleSheet.create({
     container: {
     flex: 3,
-    width: screenWidth,
-    backgroundColor:'white',
-
+    width: screenWidth-40,
+    // paddingTop:10,
+    // backgroundColor:'red',
+  alignItems: 'center',
+    
 },
   mainContainer: {
-    flex:4,
-    width: screenWidth,
-    padding:20,
-
+    flex:3,
+    width: screenWidth-40,
+  },
+  Niddle:{
+     width:'40%',
+  height:20,
+  backgroundColor:'black'
   },
   outercontainer:{
-  width:'90%',
-  height:screenHeight/2,
+    // flex:3.7,
+  width:300,
+  height:300,
   borderRadius:100*4,
-  padding:20
+  padding:20,
+  marginLeft:15,
+  alignItems: 'center',
+  marginRight:15
   },
   innercontainer:{
     backgroundColor:'white',
     width:'100%',
-    height:screenHeight/2-40,
+    height:'101%',
     borderRadius:100*4,
     padding:10
 },
@@ -160,16 +184,27 @@ imageWrapper: {
     zIndex: 10,
 },
 bottomViewleftrape:{
-  height: 27,
-  width: 27,
-backgroundColor:'#cb5949',
-borderRadius:100*4,
+  height: 18,
+  width: 19,
+backgroundColor:'#d25040',
+// '#cb5949' ,  '#d17c70'  , '#7fa0c1' , '#79aad0'
+// borderRadius:100*4,
+marginTop:-2,
+marginLeft:-2,
+borderBottomRightRadius:10,
+borderBottomLeftRadius:9,
+borderTopRightRadius:9,
 },
 bottomViewRightrape:{
-  height: 27,
-  width: 27,
+  height:18,
+  width: 19,
 backgroundColor:'#79aad0',
-borderRadius:100*4,
+// borderRadius:100*4,
+marginTop:-2,
+marginRight:-2,
+borderBottomLeftRadius:10,
+borderBottomRightRadius:9,
+borderTopLeftRadius:9
 },
   image: {
     resizeMode: 'stretch',
@@ -177,6 +212,9 @@ borderRadius:100*4,
     width: screenWidth - 80,
   },
   range:{
+    //  resizeMode: 'stretch',
+    // height: '100%',
+    // width: '100%',
     position: 'absolute',
     top: 0,
     left: 0,
@@ -184,26 +222,28 @@ borderRadius:100*4,
     borderBottomRightRadius: 0,
   },
   bottompointcontainer:{
-    height: 20,
-    // backgroundColor:'red',
-    width:screenWidth - 100,
-    marginTop:-72,
+    // flex:0.3,
+    height: 40,
+    backgroundColor:'white',
+    width: 250,
+    marginTop:-80,
     flexDirection:'row',
     zIndex: 99,
-    marginLeft:30,
-    paddingLeft:9,
-    paddingRight:6,
+    // marginLeft:50,
+    paddingLeft:-3.5,
+    paddingRight:-3.5,
+    // paddingTop:8,
     justifyContent: 'space-between',
 
 
   },
   bottomcontainer:{
-    resizeMode: 'stretch',
     height: 80,
     // position: 'absolute',
     backgroundColor:'white',
-    width:'100%',
-    marginTop:-40,
+    zIndex: 9999,
+    width: 300,
+    marginTop:-42,
     flexDirection:'row'
   },
   bottomText:{
@@ -226,5 +266,10 @@ bottomTextrape:{
 bottomMiddlerape:{
     fontSize:20,
     fontWeight:'bold'
+},
+bottomMiddlerapeText:{
+  fontSize:14,
+  fontWeight:'bold',
+  color:'gray'
 }
 });
